1ef6b03f11c5961dc1ba898e4fc9c88be0400978
Aryan Gupta <5guptaaryan10@gmail.com>
2026-04-18 21:36:08 +0530

    fix: add vercel URL to CORS allowed origins

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
+  'https://laundryf.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
